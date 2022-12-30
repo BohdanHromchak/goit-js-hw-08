e2dc2095f2fdf1ab4f1e6695b1189239783846cc
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-30 21:54:36 +0200

    updated

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,10 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Підключення бібліотеки SimpleLightbox: https://simplelightbox.com/
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// console.log(galleryItems);
+// console.log(SimpleLightbox);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,3 @@
-
+// Підключення бібліотеки vimeo: https://github.com/vimeo/player.js/#vimeo-player-api
+import Player from '@vimeo/player';
+// console.log(Player);
