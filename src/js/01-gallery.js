4d2403ebfbed43039d0a3e774cf137417cdebd1c
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-31 09:16:25 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
 // Підключення бібліотеки SimpleLightbox: https://simplelightbox.com/
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
 // console.log(galleryItems);
 // console.log(SimpleLightbox);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener('click', onClick);
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
