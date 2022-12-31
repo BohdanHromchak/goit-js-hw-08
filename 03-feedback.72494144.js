73de750e09dbc1027a5c7f40e0d086ac3d624dae
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-31 08:37:56 +0000

    Deploying to gh-pages from @ BohdanHromchak/goit-js-hw-08@0ac2669a3499d9cbf68c551e6d084dd3b06a269f 🚀

---
+++ 03-feedback.72494144.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector('[name="email"]');document.querySelector('[name="message"]');e.addEventListener("input",(function(e){console.log(e)}))}();
+//# sourceMappingURL=03-feedback.72494144.js.map
---
+++ 03-feedback.cd75a0a1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('[name="email"]');document.querySelector('[name="message"]');e.addEventListener("input",(function(e){console.log(e)}));
+//# sourceMappingURL=03-feedback.cd75a0a1.js.map
