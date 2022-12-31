9dab1e5406027d203882e766ccf6371b26e0119a
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-31 11:31:35 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,28 @@
-const email = document.querySelector('[name="email"]');
-const message = document.querySelector('[name="message"]');
+const form = document.querySelector('.feedback-form');
 
 const KEY_LOCAL = 'feedback-form-state';
 
-email.addEventListener('input', onInput);
+form.addEventListener('input', onForm);
+// message.addEventListener('input', onMessageInput);
 
-function onInput(evt) {
-  console.log(evt);
+const data = {};
+function onForm(evt) {
+  const { email, message } = evt.currentTarget.elements;
+
+  data.email = email.value;
+  data.message = message.value;
+
+  localStorage.setItem(KEY_LOCAL, JSON.stringify(data));
 }
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  form.reset();
+  localStorage.clear();
+  console.log(data);
+});
+
+function autoComplite() {
+  const storedData = JSON.parse(localStorage.getItem(KEY_LOCAL));
+  console.log(storedData);
+}
+autoComplite();
