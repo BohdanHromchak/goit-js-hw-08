0ac2669a3499d9cbf68c551e6d084dd3b06a269f
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-31 10:37:24 +0200

    updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
 
+const KEY_LOCAL = 'feedback-form-state';
+
+email.addEventListener('input', onInput);
+
+function onInput(evt) {
+  console.log(evt);
+}
